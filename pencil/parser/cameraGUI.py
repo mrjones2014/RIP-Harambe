a102415647969f64b2ed323a9652cba0f6098c96
smit-happens <gh0st-p0wer5>
2016-11-12 09:27:07 -0500

    working webcam video displaying with tkinker window frame
    need to work on image file capturing

--- pencil/parser/cameraGUI.py
+++ pencil/parser/cameraGUI.py
@@ -1,33 +1,104 @@
-from collections import deque
-import imutils
+import os
+import threading
+import tkMessageBox
+from PIL import Image
+from PIL import ImageTk
+import Tkinter as tk
+
+
 import cv2
-import Tkinter
+import imutils
+
+class imageCapture:
+    def __init__(self, vs):
+        # store the video stream object and output path, then initialize
+        # the most recently read frame, thread for reading frames, and
+        # the thread stop event
+        self.vs = vs
+        # self.outputPath = outputPath
+        self.frame = None
+        self.thread = None
+        self.stopEvent = None
+
+        # initialize the root window and image panel
+        self.root = tk.Tk()
+        self.panel = None
+
+        # create a button, that when pressed, will take the current
+        # frame and save it to file
+        btn = tk.Button(self.root, text="Snapshot!", command=self.takeSnapshot)
+        btn.pack(side="bottom", fill="both", expand="yes", padx=10, pady=10)
+
+        # start a thread that constantly pools the video sensor for
+        # the most recently read frame
+        self.stopEvent = threading.Event()
+        self.thread = threading.Thread(target=self.videoLoop, args=())
+        self.thread.start()
+
+        # set a callback to handle when the window is closed
+        self.root.wm_title("PyImageSearch PhotoBooth")
+        self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
 
 
-camera = cv2.VideoCapture(0)
+    def videoLoop(self):
+        # DISCLAIMER:
+        # I'm not a GUI developer, nor do I even pretend to be. This
+        # try/except statement is a pretty ugly hack to get around
+        # a RunTime error that Tkinter throws due to threading
+        try:
+            # keep looping over frames until we are instructed to stop
+            while not self.stopEvent.is_set():
+                # grab the frame from the video stream and resize it to
+                # have a maximum width of 300 pixels
+                _, self.frame = self.vs.read()
+                self.frame = imutils.resize(self.frame, width=300)
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-# greenLower = (29, 86, 6)
-# greenUpper = (64, 255, 255)
-# pts = deque(maxlen=args["buffer"])
+                # OpenCV represents images in BGR order; however PIL
+                # represents images in RGB order, so we need to swap
+                # the channels, then convert to PIL and ImageTk format
+                image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+                image = Image.fromarray(image)
+                image = ImageTk.PhotoImage(image)
+
+                # if the panel is not None, we need to initialize it
+                if self.panel is None:
+                    self.panel = tk.Label(image=image)
+                    self.panel.image = image
+                    self.panel.pack(side="left", padx=10, pady=10)
+
+                # otherwise, simply update the panel
+                else:
+                    self.panel.configure(image=image)
+                    self.panel.image = image
+
+        except RuntimeError, e:
+            print("[INFO] caught a RuntimeError")
 
 
-# keep looping
-while True:
-    # grab the current frame
-    (grabbed, frame) = camera.read()
+    def takeSnapshot(self):
+        filename = "test.jpg"
+        p = os.path.sep.filename
 
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    frame = imutils.resize(frame, width=600)
-    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # save the file
+        cv2.imwrite(p, self.frame.copy())
+        print("[INFO] saved {}".format(filename))
 
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    # mask = cv2.inRange(hsv, greenLower, greenUpper)
-    # mask = cv2.erode(mask, None, iterations=2)
-    # mask = cv2.dilate(mask, None, iterations=2)
+
+    def onClose(self):
+        # set the stop event, cleanup the camera, and allow the rest of
+        # the quit process to continue
+        print("[INFO] closing...")
+        self.stopEvent.set()
+        # self.vs.stop()
+        self.root.quit()
+        cv2.destroyAllWindows()
+
+vs = cv2.VideoCapture(0)
+
+cv2.waitKey(1)
+
+picture = imageCapture(vs)
+picture.root.mainloop()
+
+cv2.destroyAllWindows()
+cv2.waitKey(1)
