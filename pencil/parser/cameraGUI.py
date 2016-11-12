c052d0106f171d0737b062752828c0df394d25e6
smit-happens <gh0st-p0wer5>
2016-11-12 04:21:15 -0500

    Messing with cv2 and Tkinter

--- pencil/parser/cameraGUI.py
+++ pencil/parser/cameraGUI.py
@@ -1,3 +1,33 @@
-import numpy as np
+from collections import deque
+import imutils
 import cv2
+import Tkinter
 
+
+camera = cv2.VideoCapture(0)
+
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space, then initialize the
+# list of tracked points
+# greenLower = (29, 86, 6)
+# greenUpper = (64, 255, 255)
+# pts = deque(maxlen=args["buffer"])
+
+
+# keep looping
+while True:
+    # grab the current frame
+    (grabbed, frame) = camera.read()
+
+    # resize the frame, blur it, and convert it to the HSV
+    # color space
+    frame = imutils.resize(frame, width=600)
+    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # construct a mask for the color "green", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    # mask = cv2.inRange(hsv, greenLower, greenUpper)
+    # mask = cv2.erode(mask, None, iterations=2)
+    # mask = cv2.dilate(mask, None, iterations=2)
