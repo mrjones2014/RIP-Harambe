1df6203e7737771f6c741dd6c6a838a7ae1ed40f
Robert Jones <rjones38@ycp.edu>
2016-11-12 09:33:35 -0500

    added posting to drive functionality uh.. not much else man

--- pencil/parser/PostGui.py
+++ pencil/parser/PostGui.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
 import tkSimpleDialog
 from Tkinter import *
@@ -6,56 +8,49 @@ import tkFileDialog as fd
 import gdata.client, gdata.docs.client, gdata.docs.data, os.path, atom.data
 import tkMessageBox
 
-def saveToLocalFile(fileString):
-    file = fd.asksaveasfile()
-    file.write("fileString")
-    file.close()
-def sendToDocs():
-    docsclient = gdata.docs.client.DocsClient(source='planzero-gupload-v0.1')
 
-    # Log into Google Docs
-    onSuccess = False
-    while(onSuccess == False):
-        try:
-            username = tkSimpleDialog.askstring("Username", "Enter username:")
-            password = tkSimpleDialog.askstring("Password", "Enter password:", show='*')
-            fileName = tkSimpleDialog.askstring("FileName?", "Enter password:", show='*')
-            docsclient.ClientLogin(username, password, docsclient.source);
-        except (gdata.client.BadAuthentication, gdata.client.Error), e:
-            sys.exit('ERROR: ' + str(e))
-        except:
-            sys.exit('ERROR: Unable to login')
-        onSuccess = True
-        print
-        'success!'
-    uri = 'https://docs.google.com/feeds/upload/create-session/default/private/full'
-    fh = open(fileName, 'w')
-    fh.write(fileString)
-    fh.close()
-    file_size = os.path.getsize(fh.name)
-    #file_type = magic.Magic(mime=True).from_file(fh.name)
-    uploader = gdata.client.ResumableUploader(docsclient, fh, ".txt", file_size, chunk_size=1048576, desired_class=gdata.data.GDEntry)
-    new_entry = uploader.UploadFile(uri, entry=gdata.data.GDEntry(title=atom.data.Title(text=os.path.basename(fh.name))))
 
+
+
+
+
+window = Tk("Pencil Parser")
+
+fileString = ''
 def copyClipboard(window, fileString):
     window.clipboard_append(fileString)
 
+
+def saveToLocalFile(fileString):
+    file = fd.asksaveasfile()
+    file.write(fileString)
+    file.close()
+
+
+def sendToDrive(fileString):
+    gauth = GoogleAuth()
+    drive = GoogleDrive(gauth)
+    fileName = tkSimpleDialog.askstring("File Name", "Name of the file?")
+    f = drive.CreateFile({'title': fileName + ".txt"})
+    f.SetContentString("sup guys.")
+    f.Upload()
+
+
 class PostGui:
-    fileString = ''
     def __init__(self, file):
-
-        global fileString
+        print("1")
         self.fileString = file
-
-    window = Tk("Pencil Parser")
-    window.maxsize( 150,150)
-    window.minsize(150, 150)
-    window.resizable(width = False, height = False)
-    saveFile = Button(window, text="Save to Local File", command=saveToLocalFile(fileString)).pack()
-    saveDocs = Button(window, text="Send to Google Docs", command = sendToDocs).pack()
-    clipBoard = Button(window, text="Copy to Clipboard", command =  copyClipboard(window, fileString)).pack()
-
+        window.maxsize(150, 150)
+        window.minsize(150, 150)
+        window.resizable(width=False, height=False)
+    saveFile = Button(window, text="Save to Local File", command=lambda: saveToLocalFile(fileString)).pack()
+    saveDocs = Button(window, text="Send to Google Docs", command=lambda: sendToDrive(fileString)).pack()
+    clipBoard = Button(window, text="Copy to Clipboard", command=lambda: copyClipboard(fileString)).pack()
     window.mainloop()
+
+
+
+
 
 
 
