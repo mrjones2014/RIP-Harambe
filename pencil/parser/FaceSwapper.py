2979848b4f6a6e875d886a990fde31c1e703db84
Mat Jones <mjones44@ycp.edu>
2016-11-13 03:38:28 -0500

    Now you can swap faces with Rick Ross, Trump, and HARAMBE

--- pencil/parser/FaceSwapper.py
+++ pencil/parser/FaceSwapper.py
@@ -1,195 +1,277 @@
-import sys
-import numpy as np
 import cv2
+import dlib
+import numpy
+from pygame import mixer
+
+PREDICTOR_PATH = "/home/mat/dlib-18.16/shape_predictor_68_face_landmarks.dat"
+SCALE_FACTOR = 1
+FEATHER_AMOUNT = 11
+
+FACE_POINTS = list(range(17, 68))
+MOUTH_POINTS = list(range(48, 61))
+RIGHT_BROW_POINTS = list(range(17, 22))
+LEFT_BROW_POINTS = list(range(22, 27))
+RIGHT_EYE_POINTS = list(range(36, 42))
+LEFT_EYE_POINTS = list(range(42, 48))
+NOSE_POINTS = list(range(27, 35))
+JAW_POINTS = list(range(0, 17))
+
+# Points used to line up the images.
+ALIGN_POINTS = (LEFT_BROW_POINTS + RIGHT_EYE_POINTS + LEFT_EYE_POINTS +
+                RIGHT_BROW_POINTS + NOSE_POINTS + MOUTH_POINTS)
+
+# Points from the second image to overlay on the first. The convex hull of each
+# element will be overlaid.
+OVERLAY_POINTS = [
+    LEFT_EYE_POINTS + RIGHT_EYE_POINTS + LEFT_BROW_POINTS + RIGHT_BROW_POINTS,
+    NOSE_POINTS + MOUTH_POINTS,
+]
+
+# Amount of blur to use during colour correction, as a fraction of the
+# pupillary distance.
+COLOUR_CORRECT_BLUR_FRAC = 0.6
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(PREDICTOR_PATH)
 
 
-# Read points from text file
-def readPoints(path):
-    # Create an array of points.
-    points = [];
-
-    # Read points
-    with open(path) as file:
-        for line in file:
-            x, y = line.split()
-            points.append((int(x), int(y)))
-
-    return points
+class TooManyFaces(Exception):
+    pass
 
 
-# Apply affine transform calculated using srcTri and dstTri to src and
-# output an image of size.
-def applyAffineTransform(src, srcTri, dstTri, size):
-    # Given a pair of triangles, find the affine transform.
-    warpMat = cv2.getAffineTransform(np.float32(srcTri), np.float32(dstTri))
-
-    # Apply the Affine Transform just found to the src image
-    dst = cv2.warpAffine(src, warpMat, (size[0], size[1]), None, flags=cv2.INTER_LINEAR,
-                         borderMode=cv2.BORDER_REFLECT_101)
-
-    return dst
+class NoFaces(Exception):
+    pass
 
 
-# Check if a point is inside a rectangle
-def rectContains(rect, point):
-    if point[0] < rect[0]:
+def get_landmarks(im):
+    rects = detector(im, 1)
+
+    if len(rects) > 1:
+        raise TooManyFaces
+    if len(rects) == 0:
+        raise NoFaces
+
+    return numpy.matrix([[p.x, p.y] for p in predictor(im, rects[0]).parts()])
+
+
+def annotate_landmarks(im, landmarks):
+    im = im.copy()
+    for idx, point in enumerate(landmarks):
+        pos = (point[0, 0], point[0, 1])
+        cv2.putText(im, str(idx), pos,
+                    fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+                    fontScale=0.4,
+                    color=(0, 0, 255))
+        cv2.circle(im, pos, 3, color=(0, 255, 255))
+    return im
+
+
+def draw_convex_hull(im, points, color):
+    points = cv2.convexHull(points)
+    cv2.fillConvexPoly(im, points, color=color)
+
+
+def get_face_mask(im, landmarks):
+    im = numpy.zeros(im.shape[:2], dtype=numpy.float64)
+
+    for group in OVERLAY_POINTS:
+        draw_convex_hull(im,
+                         landmarks[group],
+                         color=1)
+
+    im = numpy.array([im, im, im]).transpose((1, 2, 0))
+
+    im = (cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0) > 0) * 1.0
+    im = cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0)
+
+    return im
+
+
+def transformation_from_points(points1, points2):
+    """
+    Return an affine transformation [s * R | T] such that:
+        sum ||s*R*p1,i + T - p2,i||^2
+    is minimized.
+    """
+    # Solve the procrustes problem by subtracting centroids, scaling by the
+    # standard deviation, and then using the SVD to calculate the rotation. See
+    # the following for more details:
+    #   https://en.wikipedia.org/wiki/Orthogonal_Procrustes_problem
+
+    points1 = points1.astype(numpy.float64)
+    points2 = points2.astype(numpy.float64)
+
+    c1 = numpy.mean(points1, axis=0)
+    c2 = numpy.mean(points2, axis=0)
+    points1 -= c1
+    points2 -= c2
+
+    s1 = numpy.std(points1)
+    s2 = numpy.std(points2)
+    points1 /= s1
+    points2 /= s2
+
+    U, S, Vt = numpy.linalg.svd(points1.T * points2)
+
+    # The R we seek is in fact the transpose of the one given by U * Vt. This
+    # is because the above formulation assumes the matrix goes on the right
+    # (with row vectors) where as our solution requires the matrix to be on the
+    # left (with column vectors).
+    R = (U * Vt).T
+
+    return numpy.vstack([numpy.hstack(((s2 / s1) * R,
+                                       c2.T - (s2 / s1) * R * c1.T)),
+                         numpy.matrix([0., 0., 1.])])
+
+
+def read_im_and_landmarks(fname):
+    im = fname
+    im = cv2.resize(im, (im.shape[1] * SCALE_FACTOR,
+                         im.shape[0] * SCALE_FACTOR))
+    s = get_landmarks(im)
+
+    return im, s
+
+
+def warp_im(im, M, dshape):
+    output_im = numpy.zeros(dshape, dtype=im.dtype)
+    cv2.warpAffine(im,
+                   M[:2],
+                   (dshape[1], dshape[0]),
+                   dst=output_im,
+                   borderMode=cv2.BORDER_TRANSPARENT,
+                   flags=cv2.WARP_INVERSE_MAP)
+    return output_im
+
+
+def correct_colours(im1, im2, landmarks1):
+    blur_amount = COLOUR_CORRECT_BLUR_FRAC * numpy.linalg.norm(
+        numpy.mean(landmarks1[LEFT_EYE_POINTS], axis=0) -
+        numpy.mean(landmarks1[RIGHT_EYE_POINTS], axis=0))
+    blur_amount = int(blur_amount)
+    if blur_amount % 2 == 0:
+        blur_amount += 1
+    im1_blur = cv2.GaussianBlur(im1, (blur_amount, blur_amount), 0)
+    im2_blur = cv2.GaussianBlur(im2, (blur_amount, blur_amount), 0)
+
+    # Avoid divide-by-zero errors.
+    im2_blur += (128 * (im2_blur <= 1.0)).astype(im2_blur.dtype)
+
+    return (im2.astype(numpy.float64) * im1_blur.astype(numpy.float64) /
+            im2_blur.astype(numpy.float64))
+
+
+def overlay(s_img, l_img, x, y):
+    x_offset = x
+    y_offset = y
+    name = s_img
+    if s_img == "harambe.png":
+        x_offset -= 30
+        y_offset -= 100
+    elif s_img == "rickross.png":
+        x_offset -= 45
+        y_offset -= 140
+    elif s_img == "trump.png":
+        x_offset -= 80
+        y_offset -= 125
+
+    s_img = cv2.imread(s_img, -1)
+    try:
+        for c in range(0, 3):
+            l_img[y_offset:y_offset + s_img.shape[0], x_offset:x_offset + s_img.shape[1], c] = s_img[:, :, c] * (s_img[:, :, 3] / 255.0) + l_img[y_offset:y_offset + s_img.shape[0],
+                                                        x_offset:x_offset + s_img.shape[1], c] * (
+                                                        1.0 - s_img[:, :, 3] / 255.0)
+        return True
+    except Exception, e:
+        if name == "harambe.png":
+            cv2.putText(l_img, "RIP Harambe!", (x_offset, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+        else:
+            print e.message
         return False
-    elif point[1] < rect[1]:
-        return False
-    elif point[0] > rect[0] + rect[2]:
-        return False
-    elif point[1] > rect[1] + rect[3]:
-        return False
-    return True
 
 
-# calculate delanauy triangle
-def calculateDelaunayTriangles(rect, points):
-    # create subdiv
-    subdiv = cv2.Subdiv2D(rect);
-
-    # Insert points into subdiv
-    for p in points:
-        subdiv.insert(p)
-
-    triangleList = subdiv.getTriangleList();
-
-    delaunayTri = []
-
-    pt = []
-
-    count = 0
-
-    for t in triangleList:
-        pt.append((t[0], t[1]))
-        pt.append((t[2], t[3]))
-        pt.append((t[4], t[5]))
-
-        pt1 = (t[0], t[1])
-        pt2 = (t[2], t[3])
-        pt3 = (t[4], t[5])
-
-        if rectContains(rect, pt1) and rectContains(rect, pt2) and rectContains(rect, pt3):
-            count = count + 1
-            ind = []
-            for j in xrange(0, 3):
-                for k in xrange(0, len(points)):
-                    if (abs(pt[j][0] - points[k][0]) < 1.0 and abs(pt[j][1] - points[k][1]) < 1.0):
-                        ind.append(k)
-            if len(ind) == 3:
-                delaunayTri.append((ind[0], ind[1], ind[2]))
-
-        pt = []
-
-    return delaunayTri
+def harambeface(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+    success = True
+    # Draw a harambe on the faces
+    for (x, y, w, h) in faces:
+        success = overlay("harambe.png", img, x, y)
+    return success
 
 
-# Warps and alpha blends triangular regions from img1 and img2 to img
-def warpTriangle(img1, img2, t1, t2):
-    # Find bounding rectangle for each triangle
-    r1 = cv2.boundingRect(np.float32([t1]))
-    r2 = cv2.boundingRect(np.float32([t2]))
-
-    # Offset points by left top corner of the respective rectangles
-    t1Rect = []
-    t2Rect = []
-    t2RectInt = []
-
-    for i in xrange(0, 3):
-        t1Rect.append(((t1[i][0] - r1[0]), (t1[i][1] - r1[1])))
-        t2Rect.append(((t2[i][0] - r2[0]), (t2[i][1] - r2[1])))
-        t2RectInt.append(((t2[i][0] - r2[0]), (t2[i][1] - r2[1])))
-
-    # Get mask by filling triangle
-    mask = np.zeros((r2[3], r2[2], 3), dtype=np.float32)
-    cv2.fillConvexPoly(mask, np.int32(t2RectInt), (1.0, 1.0, 1.0), 16, 0);
-
-    # Apply warpImage to small rectangular patches
-    img1Rect = img1[r1[1]:r1[1] + r1[3], r1[0]:r1[0] + r1[2]]
-    # img2Rect = np.zeros((r2[3], r2[2]), dtype = img1Rect.dtype)
-
-    size = (r2[2], r2[3])
-
-    img2Rect = applyAffineTransform(img1Rect, t1Rect, t2Rect, size)
-
-    img2Rect = img2Rect * mask
-
-    # Copy triangular region of the rectangular patch to the output image
-    img2[r2[1]:r2[1] + r2[3], r2[0]:r2[0] + r2[2]] = img2[r2[1]:r2[1] + r2[3], r2[0]:r2[0] + r2[2]] * (
-    (1.0, 1.0, 1.0) - mask)
-
-    img2[r2[1]:r2[1] + r2[3], r2[0]:r2[0] + r2[2]] = img2[r2[1]:r2[1] + r2[3], r2[0]:r2[0] + r2[2]] + img2Rect
+def blockface(img, r, g, b):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x - 20, y - 20), (x + w + 20, y + h + 20), (b, g, r), thickness=cv2.cv.CV_FILLED)
 
 
-if __name__ == '__main__':
+def trumpface(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+    success = True
+    # Draw a harambe on the faces
+    for (x, y, w, h) in faces:
+        success = overlay("trump.png", img, x, y)
+    return success
 
-    # Make sure OpenCV is version 3.0 or above
-    (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
 
-    if int(major_ver) < 3:
-        print >> sys.stderr, 'ERROR: Script needs OpenCV 3.0 or higher'
-        sys.exit(1)
+def rickrossface(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=15,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+    success = True
+    # Draw a harambe on the faces
+    for (x, y, w, h) in faces:
+        success = overlay("rickross.png", img, x, y)
+    return success
 
-    # Read images
-    filename1 = 'ted_cruz.jpg'
-    filename2 = 'donald_trump.jpg'
 
-    img1 = cv2.imread(filename1);
-    img2 = cv2.imread(filename2);
-    img1Warped = np.copy(img2);
-
-    # Read array of corresponding points
-    points1 = readPoints(filename1 + '.txt')
-    points2 = readPoints(filename2 + '.txt')
-
-    # Find convex hull
-    hull1 = []
-    hull2 = []
-
-    hullIndex = cv2.convexHull(np.array(points2), returnPoints=False)
-
-    for i in xrange(0, len(hullIndex)):
-        hull1.append(points1[hullIndex[i]])
-        hull2.append(points2[hullIndex[i]])
-
-    # Find delanauy traingulation for convex hull points
-    sizeImg2 = img2.shape
-    rect = (0, 0, sizeImg2[1], sizeImg2[0])
-
-    dt = calculateDelaunayTriangles(rect, hull2)
-
-    if len(dt) == 0:
-        quit()
-
-    # Apply affine transformation to Delaunay triangles
-    for i in xrange(0, len(dt)):
-        t1 = []
-        t2 = []
-
-        # get points for img1, img2 corresponding to the triangles
-        for j in xrange(0, 3):
-            t1.append(hull1[dt[i][j]])
-            t2.append(hull2[dt[i][j]])
-
-        warpTriangle(img1, img1Warped, t1, t2)
-
-    # Calculate Mask
-    hull8U = []
-    for i in xrange(0, len(hull2)):
-        hull8U.append((hull2[i][0], hull2[i][1]))
-
-    mask = np.zeros(img2.shape, dtype=img2.dtype)
-
-    cv2.fillConvexPoly(mask, np.int32(hull8U), (255, 255, 255))
-
-    r = cv2.boundingRect(np.float32([hull2]))
-
-    center = ((r[0] + int(r[2] / 2), r[1] + int(r[3] / 2)))
-
-    # Clone seamlessly.
-    output = cv2.seamlessClone(np.uint8(img1Warped), img2, mask, center, cv2.NORMAL_CLONE)
-
-    cv2.imshow("Face Swapped", output)
-    cv2.waitKey(0)
-
-    cv2.destroyAllWindows()
+lastFrame = None
+camera = cv2.VideoCapture(0)
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+mixer.init()
+mixer.music.load("rick_ross_push_it.ogg")
+mixer.music.play()
+while True:
+    _, img = camera.read()
+    success = rickrossface(img)
+    if success:
+        mixer.music.unpause()
+        #cv2.imshow("win", img)
+        #lastFrame = img
+    else:
+        mixer.music.pause();
+        #if lastFrame is not None:
+         #   cv2.imshow("win", lastFrame)
+        #else:
+         #   cv2.imshow("win", img)
+    cv2.imshow("win", img)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
